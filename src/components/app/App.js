9b0369a61d13b06030798c0acef998e165e93041
johnmckenna-snd <63067728+johnmckenna-snd@users.noreply.github.com>
2020-11-12 15:59:19 -0800

    ok it works time for movie and pizza

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import CountdownTimer from '../countdown-timer/CountdownTimer';
 
 const App = () => {
+	const [timers, setTimers] = useState([]);
+	const [timerkey, setTimerKey] = useState(1);
+
+	const addTimer = (time) => {
+		const newTimers = [...timers, <CountdownTimer timerKey={timerkey} />];
+		setTimerKey(timerkey + 1);
+		setTimers(newTimers);
+	};
+
 	return (
 		<div>
-			<h1>app</h1>
-			<CountdownTimer />
+			<h1>kairos</h1>
+			<button onClick={addTimer}>new timer</button>
+			{timers}
 		</div>
 	);
 };
--- src/components/countdown-timer/CountdownTimer.js
+++ src/components/countdown-timer/CountdownTimer.js
@@ -1,16 +1,24 @@
 import Countdown from 'react-countdown';
 
-const CountdownTimer = () => {
-	const renderer = ({ hours, minutes, seconds, completed }) => {
-		if (completed) return <h1>yay</h1>;
-		return <h1>{hours < 10 ? `0${hours}` : hours}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>;
+const CountdownTimer = ({timerKey}) => {
+	const renderer = ({ hours, minutes, seconds, completed, api }) => {
+		if (completed) return <h1 key={timerKey}>{timerKey}</h1>;
+		return (
+			<div>
+				<h1 key={timerKey}>{hours < 10 ? `0${hours}` : hours}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+				<span>
+					<button onClick={api.start}>start</button>
+					<button onClick={api.pause}>pause</button>
+				</span>
+			</div>
+		);
 	};
 	return (
 		<Countdown
 			date={Date.now() + 10000}
 			renderer={renderer}
 			/>
-	)
+	);
 };
 
 export default CountdownTimer;
